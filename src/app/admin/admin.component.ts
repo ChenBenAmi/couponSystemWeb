2c16b52306abf0563a21add751ac712d5084167e
Chen Ben-Ami <chen24201@gmail.com>
2019-08-04 23:31:29 +0300

    Update admin.component.ts

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -12,12 +12,20 @@ import { HttpClient, JsonpClientBackend } from '@angular/common/http';
 
 export class AdminComponent implements OnInit {
   private url = 'http://localhost:8080/Coupon_System/rest/admin/getCompanies';
+  private url2='http://localhost:8080/Coupon_System/rest/admin/getCustomers';
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.http.get(this.url, { withCredentials: true })
       .subscribe(
-        (data: any[]) => {
+        (data: any) => {
+          console.log(data);
+        }
+      );
+
+    this.http.get(this.url2, { withCredentials: true })
+      .subscribe(
+        (data: any) => {
           console.log(data);
         }
       );
