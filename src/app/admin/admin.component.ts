ab899227da4bd526b24e229570bde2070592519d
Adiivgi <46651329+Adiivgi@users.noreply.github.com>
2019-08-04 16:57:30 +0300

    Httprequest Admin work!

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient, JsonpClientBackend } from '@angular/common/http';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements OnInit {
 
-  constructor() { }
+  
+
+
+ 
+export class AdminComponent implements OnInit {
+private _url:string = "http://localhost:8080/Coupon_System/rest/admin/getCompanies";
+private example = "https://my-json-server.typicode.com/typicode/demo/profile";
+constructor(private http: HttpClient) { }
 
   ngOnInit() {
+   this.http.get(this._url)
+   .subscribe(
+     (data:any[])=>{
+       console.log(data);
+     }
+   );
   }
-
 }
