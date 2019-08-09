00881a7e991b41f321f7a3c0297772d16cd9372d
Adiivgi <46651329+Adiivgi@users.noreply.github.com>
2019-08-09 08:48:20 +0300

    admin componet html problem

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, JsonpClientBackend } from '@angular/common/http';
+import { Company } from './companies/company';
+  import { from } from 'rxjs';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -8,21 +10,21 @@ import { HttpClient, JsonpClientBackend } from '@angular/common/http';
 
 
 
-
-
 export class AdminComponent implements OnInit {
   private url = 'http://localhost:8080/Coupon_System/rest/admin/getCompanies';
   private url2='http://localhost:8080/Coupon_System/rest/admin/getCustomers';
+  private companies : Company[] = [];
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.http.get(this.url, { withCredentials: true })
       .subscribe(
-        (data: any) => {
+        (data: Company[]) => {
           console.log(data);
+          this.companies = data;
         }
       );
-
+    
     this.http.get(this.url2, { withCredentials: true })
       .subscribe(
         (data: any) => {
